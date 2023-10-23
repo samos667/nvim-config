299adc87c892ef992bc7ff73a0f606cc9b7f44db
ekip <sammyetur11@gmail.com>
2023-10-23 17:36:19 +0000

    fix

--- keymap/ui.lua
+++ keymap/ui.lua
@@ -3,5 +3,5 @@ local map_cr = bind.map_cr
 
 return {
 	["n|<Tab>"] = map_cr("BufferLineCycleNext"):with_noremap():with_silent():with_desc("buffer: Switch to next"),
-	["n|<S-Tab>"] = map_cr("BufferLineCycleNextPrev"):with_noremap():with_silent():with_desc("buffer: Switch to next"),
+	["n|<S-Tab>"] = map_cr("BufferLineCyclePrev"):with_noremap():with_silent():with_desc("buffer: Switch to next"),
 }
